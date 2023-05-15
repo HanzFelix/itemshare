e430187fc47a795744d9eeca98e7d38e6ced43f4
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-05-16 07:23:50 +0800

    Add auto-hide navbar on login

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const itemShareStore = useItemShareStore();
 
 <template>
   <!-- Temporary header to demonstrate vue router -->
-  <header class="bg-green-500 py-2 px-8 flex gap-4">
+  <header class="bg-green-500 py-2 px-8 flex gap-4 fixed w-full" v-if="!($route.name).includes('login')">
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/item">Item Detail</RouterLink>
     <RouterLink to="/login">Login</RouterLink>
