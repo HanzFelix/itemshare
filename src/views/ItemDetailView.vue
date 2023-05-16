16e25ca88992e66cb47f8ded21734640dca5094a
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-05-16 13:46:39 +0800

    Update ItemDetailView.vue

--- src/views/ItemDetailView.vue
+++ src/views/ItemDetailView.vue
@@ -1,4 +1,59 @@
-
 <template>
-    itemdetail
+  <main class="flex flex-col pt-40 container mx-auto px-4 gap-8">
+    <section class="flex flex-row-reverse gap-2">Close X</section>
+    <section class="flex gap-2">
+      <!--Images-->
+      <div class="w-96 flex flex-col gap-2">
+        <img
+          src="https://www.ikea.com/ph/en/images/products/ringsta-lamp-shade-white__0784061_pe761617_s5.jpg"
+          alt=""
+          srcset=""
+        />
+        <div class="flex w-full gap-2">
+          <img
+            class="w-20 aspect-auto"
+            src="https://www.ikea.com/ph/en/images/products/ringsta-lamp-shade-white__0784061_pe761617_s5.jpg"
+            alt=""
+            srcset=""
+          />
+          <img
+            class="w-20 aspect-auto rotate-90"
+            src="https://www.ikea.com/ph/en/images/products/ringsta-lamp-shade-white__0784061_pe761617_s5.jpg"
+            alt=""
+            srcset=""
+          />
+          <img
+            class="w-20 aspect-auto rotate-180"
+            src="https://www.ikea.com/ph/en/images/products/ringsta-lamp-shade-white__0784061_pe761617_s5.jpg"
+            alt=""
+            srcset=""
+          />
+        </div>
+      </div>
+      <!--Item Description-->
+      <div class="flex flex-col justify-between w-full">
+        <div>
+          <p>AVAILABLE</p>
+          <h1>3D Scanner</h1>
+          <p>P 150 - per week</p>
+          <div>4.0 / 5.0 ⭐⭐⭐⭐⭐</div>
+          <p>Baybay City</p>
+        </div>
+        <div class="flex flex-col">
+          <div>
+            Tags:
+            <span>scanner</span>
+            <span>3d</span>
+            <span>technology</span>
+            <span>gadget</span>
+            <span>device</span>
+          </div>
+          <button>Rent</button>
+          <button>Add to Favorites</button>
+        </div>
+      </div>
+      <!--User Description-->
+      <div>user</div>
+    </section>
+  </main>
 </template>
