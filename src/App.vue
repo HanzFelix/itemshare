6df0eb729947266192633d22f4abdc85f2a8f773
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-05-16 09:32:24 +0800

    Format code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { useItemShareStore } from './stores/itemshare'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import { useItemShareStore } from "./stores/itemshare";
 
 const itemShareStore = useItemShareStore();
 </script>
 
 <template>
   <!-- Temporary header to demonstrate vue router -->
-  <header class="bg-green-500 py-2 px-8 flex gap-4 fixed w-full" v-if="!($route.name).includes('login')">
+  <header
+    class="bg-green-500 py-2 px-8 flex gap-4 fixed w-full"
+    v-if="!['login'].includes($route.name)"
+  >
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/item">Item Detail</RouterLink>
     <RouterLink to="/login">Login</RouterLink>
