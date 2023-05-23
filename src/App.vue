a54786d57790099dae362a6017fb23a14962c3a2
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-05-23 09:36:14 +0800

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router";
 import NavigationBar from "./components/NavigationBar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <NavigationBar />
   <RouterView />
+  <Footer />
 </template>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,2 +1,13 @@
 <script setup></script>
-<template></template>
+<template>
+  <header
+    class="bg-green-600 flex flex-col fixed w-full text-white bottom-0"
+    v-if="!['login', 'register'].includes($route.name)"
+  >
+    <nav
+      class="flex items-center justify-end container mx-auto py-4 bg-green-600"
+    >
+      <p>All Rights Reserved 2023. &reg;</p>
+    </nav>
+  </header>
+</template>
