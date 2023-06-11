31cfe4da8a730aaad018c149c641992e8eeafe90
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-06-12 06:57:31 +0800

    Add slot

--- src/views/EmptyView.vue
+++ src/views/EmptyView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="h-full flex flex-col justify-center items-center">
-    <p class="text-gray-500">Click a conversation to start</p>
+    <p class="text-gray-500"><slot>Click a conversation to start</slot></p>
   </div>
 </template>
