aecfdcf0c6c40afa5efddeb06102e19d803bafd2
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-06-13 09:27:58 +0800

    Disable text selection

--- src/components/ItemShareLogo.vue
+++ src/components/ItemShareLogo.vue
@@ -5,7 +5,7 @@ import itemShareLogo from "../assets/ItemShareLogo.png";
 <template>
   <RouterLink to="/" class="flex gap-2 items-center">
     <span
-      class="text-3xl text-white font-poppins font-black underline decoration-green-900 underline-offset-4 decoration-4 md:inline-block hidden"
+      class="text-3xl text-white font-poppins font-black underline decoration-green-900 underline-offset-4 decoration-4 md:inline-block hidden select-none"
       >ItemShare</span
     >
     <img :src="itemShareLogo" class="w-10 rounded-xl" loading="lazy"
