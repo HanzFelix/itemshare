25584fc1017e96064619cd8e08be3dfd0a19e7cd
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-06-19 16:58:23 +0800

    Add flowbite package

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 import { RouterView } from "vue-router";
 import NavigationBar from "./components/NavigationBar.vue";
 import Footer from "./components/Footer.vue";
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+});
 </script>
 
 <template>
