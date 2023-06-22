84dd220f31c9076f03fbebbca06ace5e91fafc87
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-06-22 13:57:41 +0800

    Remove typechecking

--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -2,13 +2,12 @@
 const props = defineProps({
   // value is the number of stars
   value: {
-    type: Number,
     default: 0,
   },
 });
 </script>
 <template>
-  <p class="text-green-600 whitespace-pre">
+  <p class="whitespace-pre text-green-600">
     <span v-for="i in 5" class="material-icons-outlined">
       {{ i < value ? "star" : "grade" }}
     </span>
