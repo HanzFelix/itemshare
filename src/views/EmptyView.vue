a46acec226c66406c6138c42c7aede8dddeb4175
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-06-22 10:11:19 +0800

    Updates

--- src/views/EmptyView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="h-full flex flex-col justify-center items-center">
-    <p class="text-gray-500"><slot>Click a conversation to start</slot></p>
-  </div>
-</template>
--- src/views/RequestItemView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="h-full flex flex-col justify-center items-center">
-    <p class="text-gray-500 select-none">
-      Might want to get the devs to work on this soon...
-    </p>
-  </div>
-</template>
