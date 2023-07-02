0cfa3bf02e14133c23ee6066431032d5e63a2b27
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-07-02 18:48:20 +0800

    Add scroll to top

--- src/router/index.js
+++ src/router/index.js
@@ -89,6 +89,13 @@ const router = createRouter({
       ],
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 // checks if user is logged in before navigating
