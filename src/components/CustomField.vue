b5d7bc44051c58f7371ea64b5162b28fdf3c91ab
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-07-02 18:45:27 +0800

    add disabled for input

--- src/components/CustomField.vue
+++ src/components/CustomField.vue
@@ -4,6 +4,7 @@ defineProps({
   inputType: { type: String, default: "text" },
   placeholder: { type: String, default: "" },
   label: { type: String, default: "" },
+  disabled: { default: false },
 });
 
 // defines what events our component emits
@@ -22,8 +23,9 @@ defineEmits(["update:modelValue"]);
       :type="inputType"
       :placeholder="placeholder"
       :value="modelValue"
+      :disabled="disabled"
       @input="$emit('update:modelValue', $event.target.value)"
-      class="rounded-md border-2 border-text border-opacity-50 bg-white bg-opacity-50 text-text placeholder:text-sm placeholder:text-text placeholder:text-opacity-60 focus:border-2 focus:border-primary focus:border-opacity-100 focus:bg-white focus:bg-opacity-90 focus:ring-0 focus:placeholder:text-text focus:placeholder:text-opacity-50"
+      class="rounded-md border-2 border-text border-opacity-50 bg-white bg-opacity-50 text-text placeholder:text-sm placeholder:text-text placeholder:text-opacity-60 focus:border-2 focus:border-primary focus:border-opacity-100 focus:bg-white focus:bg-opacity-90 focus:ring-0 focus:placeholder:text-text focus:placeholder:text-opacity-50 disabled:bg-text disabled:bg-opacity-10"
     />
     <slot></slot>
   </div>
