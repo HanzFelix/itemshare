836465094af36c0080fffb063a34fffd645ef05c
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-07-07 01:36:14 +0800

    Remove unused comments

--- src/views/ConversationView.vue
+++ src/views/ConversationView.vue
@@ -1,37 +1,20 @@
 <script setup>
-import { onMounted, onUpdated, ref, watch } from "vue";
+import { onMounted, ref, watch } from "vue";
 import { useRoute, RouterLink } from "vue-router";
-import CustomField from "../components/CustomField.vue";
-import { useItemShareStore } from "../stores/itemshare";
+import { useItemShareStore } from "@/stores/itemshare";
+import CustomField from "@/components/CustomField.vue";
+
 const route = useRoute();
 const itemShareStore = useItemShareStore();
+const convoId = ref(route.params.id);
 
 const messageDraft = ref("");
-const scrollToElement = ref();
 const otherParticipant = ref(itemShareStore.tempUserProfile);
 const conversationDetails = ref({
   lastMessage: "Loading...",
   lastSender: "12",
   isRead: true,
 });
-const convoId = ref(route.params.id);
-
-const isLoading = ref(true);
-const messages = ref([]);
-
-onMounted(async () => {
-  await loadData(route.params.id);
-});
-
-watch(
-  () => route.params.id,
-  async (newId) => {
-    if (newId) {
-      convoId.value = newId;
-      await loadData(newId);
-    }
-  }
-);
 
 async function loadData(convoId) {
   conversationDetails.value = await itemShareStore.loadConversation(convoId);
@@ -52,50 +35,21 @@ async function sendMessage(message) {
     type: "message",
   });
   await itemShareStore.sendMessage(convoId.value, message);
-  //scrollToElement.value.scrollTop = scrollContainer.value.scrollHeight;
-}
-/*
-// hide avatar if from same sender
-function isSamePerson(chat_id) {
-  if (chat_id + 1 == messages.value.length) {
-    return false;
-  }
-
-  return messages.value[chat_id].sender == messages.value[chat_id + 1].sender;
 }
 
-// stylize chat bubbles
-function isSameMessageSource(chat_id) {
-  var classes = "";
+onMounted(async () => {
+  await loadData(route.params.id);
+});
 
-  if (
-    chat_id + 1 < messages.value.length &&
-    messages.value[chat_id].sender == messages.value[chat_id + 1].sender
-  ) {
-    classes +=
-      messages.value[chat_id].sender == itemShareStore.myUserUid
-        ? " rounded-br-none"
-        : " rounded-bl-none";
+watch(
+  () => route.params.id,
+  async (newId) => {
+    if (newId) {
+      convoId.value = newId;
+      await loadData(newId);
+    }
   }
-
-  if (
-    chat_id - 1 >= 0 &&
-    messages.value[chat_id].sender == messages.value[chat_id - 1].sender
-  ) {
-    classes +=
-      messages.value[chat_id].sender == itemShareStore.myUserUid
-        ? " rounded-tr-none"
-        : " rounded-tl-none";
-  }
-
-  // use green color if message source is me
-  classes +=
-    messages.value[chat_id].sender == itemShareStore.myUserUid
-      ? " bg-primary text-background bg-opacity-90"
-      : " bg-secondary bg-opacity-40";
-
-  return classes;
-}*/
+);
 </script>
 <template>
   <div
@@ -126,10 +80,7 @@ function isSameMessageSource(chat_id) {
       >
     </header>
     <!--Chat bubbles-->
-    <main
-      class="flex basis-full flex-col-reverse overflow-y-auto"
-      ref="scrollToElement"
-    >
+    <main class="flex basis-full flex-col-reverse overflow-y-auto">
       <div class="bg-white px-6 pt-12">
         <div
           v-for="(message, index) in itemShareStore.loadedMessages"
